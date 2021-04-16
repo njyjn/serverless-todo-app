3eb13cfadbf75a69a2013fd49a4ad6b9f515890f
Justin Ng <justin@aletheon.co>
2021-04-15 23:03:29 -0700

    Implement generate upload url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,51 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createLogger } from '../../utils/logger';
+import * as AWS from 'aws-sdk';
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+});
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const signedUrlExpiration = process.env.SIGNED_URL_EXPIRATION;
+const bucketName = process.env.TODOS_S3_BUCKET;
 
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+const logger = createLogger('getTodos');
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+  try {
+    const userId = event.requestContext.authorizer.principalId;
+    const signedUrl = await getUploadUrl(userId, todoId);
+    logger.info(`Processed request for ${userId} to generate signed url`, signedUrl);
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        uploadUrl: signedUrl
+      })
+    }
+  } catch (e) {
+    logger.info('Processed request but failed', e);
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: e
+      })
+    }
+  }
+}
+
+async function getUploadUrl(userId: string, todoId: string): Promise<string> {
+    return await s3.getSignedUrlPromise('putObject', {
+        Bucket: bucketName,
+        Key: `${userId}/${todoId}.png`,
+        Expires: parseInt(signedUrlExpiration),
+    });
+};
--- backend/src/logic/todos.ts
+++ backend/src/logic/todos.ts
@@ -5,17 +5,21 @@ import { v4 as uuidv4 } from 'uuid';
 import { UpdateTodoRequest } from 'src/requests/UpdateTodoRequest';
 
 const todoAccess = new TodoAccess();
+const bucketName = process.env.TODOS_S3_BUCKET;
+const attachmentBaseUrl = `https://${bucketName}.s3.amazonaws.com`
 
 export async function getTodos(userId: string) {
     return await todoAccess.getTodos(userId);
 }
 
 export async function createTodo(userId: string, newTodo: CreateTodoRequest): Promise<TodoItem> {
+    const todoId = uuidv4();
     const newItem = {
         userId: userId,
-        todoId: uuidv4(),
+        todoId: todoId,
         createdAt: new Date().toISOString(),
         done: false,
+        attachmentUrl: `${attachmentBaseUrl}/${userId}/${todoId}.png`,
         ...newTodo
     } as TodoItem;
     return await todoAccess.createTodo(newItem);
